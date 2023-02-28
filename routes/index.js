cf3684f17cfdfa3fcefac3d7d3d85c4206ae173c
Daniel Pyo <tailer9999@gmail.com>
2023-02-28 17:38:59 +0900

    add jwtoken method

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,58 @@ const express = require("express");
 const axios = require("axios");
 
 const router = express.Router();
+const URL = "http://localhost:8002/v2";
+axios.defaults.headers.origin = "http://localhost:4000";
+
+const request = async (req, api) => {
+  try {
+    if (!req.session.jwt) {
+      const tokenResult = await axios.post(`${URL}/token`, {
+        clientSecret: process.env.CLIENT_SECRET,
+      });
+      req.session.jwt = tokenResult.data.token;
+    }
+    return await axios.get(`${URL}${api}`, {
+      headers: { authorization: req.session.jwt },
+    });
+  } catch (error) {
+    if (error.response.status === 419) {
+      delete req.session.jwt;
+      return request(req, api);
+    }
+    return error.response;
+  }
+};
+
+router.get("/mypost", async (req, res, next) => {
+  try {
+    const result = await request(req, "/posts/my");
+    res.json(result.data);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.get("/search/:hashtag", async (req, res, next) => {
+  try {
+    const result = await request(
+      req,
+      `/posts/hashtag/${encodeURIComponent(req.params.hashtag)}`
+    );
+    res.json(result.data);
+  } catch (error) {
+    if (error.code) {
+      console.error(error);
+      next(error);
+    }
+  }
+});
 
 router.get("/test", async (req, res, next) => {
   try {
     if (!req.session.jwt) {
-      const tokenResult = await axios.post("http://localhost:8002/v1/token", {
+      const tokenResult = await axios.post("http://localhost:8002/v2/token", {
         clientSecret: process.env.CLIENT_SECRET,
       });
       if (tokenResult.data && tokenResult.data.code === 200) {
@@ -16,7 +63,7 @@ router.get("/test", async (req, res, next) => {
       }
     }
 
-    const result = await axios.get("http://localhost:8002/v1/test", {
+    const result = await axios.get("http://localhost:8002/v2/test", {
       headers: { authorization: req.session.jwt },
     });
     return res.json(result.data);
@@ -27,6 +74,10 @@ router.get("/test", async (req, res, next) => {
     }
     return next(error);
   }
+});
+
+router.get("/", (req, res) => {
+  res.render("main", { key: process.env.CLIENT_SECRET });
 });
 
 module.exports = router;
